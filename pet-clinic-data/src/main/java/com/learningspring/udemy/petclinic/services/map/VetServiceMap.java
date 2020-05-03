d28541cf03bde5ef9046aadb7b20b5140acff54f
abhishek kumar <abhishek.kumar@247.ai>
2020-05-03 22:45:52 +0530

    Listing all vets on vet index page closes #15

--- pet-clinic-data/src/main/java/com/learningspring/udemy/petclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/learningspring/udemy/petclinic/services/map/VetServiceMap.java
@@ -8,7 +8,7 @@ import java.util.Set;
 @Service
 public class VetServiceMap extends AbstractMapService<Vet , Long> implements VetService {
     @Override
-    public Set<Vet > findAll() {
+    public Set<Vet> findAll() {
         return super.findAll();
     }
 
